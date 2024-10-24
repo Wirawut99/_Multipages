5c98a41d54816711165ad0293e7f758e9afe73e0
Wirawut99 <wirawut.che@spumail.net>
2024-10-24 22:53:42 +0700

    va

--- src/pagess/Home/Home.jsx
+++ src/pagess/Home/Home.jsx
@@ -3,10 +3,8 @@ import React from 'react';
 function Home() {
 	return (
 		<div className='profile-container'>
-			<img
-				src='https://studentscms.spu.ac.th/stdempimg.cfm?empstdtype=STD&vdata=0AD0CFEDD585F6A50528CED5D804C3D482E1170BD5FED7D1'
-				alt=''
-			/>
+			<img src='public/stdempimg.jpg' alt='Student Image' />
+
 			<div className='profile-info'>
 				<h2>ชื่อ:นาย วีรวุฒิ ชีอยู่ รหัส 66037290</h2>
 				<p>
